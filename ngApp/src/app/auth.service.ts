5b5aae38a8ff2ff6f2d9b7faf7fb71efb9119fe9
MolnarArthur <molnararthur26@gmail.com>
2018-05-28 10:25:12 +0300

    Set Token in localStorage

--- ngApp/src/app/auth.service.ts
+++ ngApp/src/app/auth.service.ts
@@ -17,4 +17,9 @@ export class AuthService {
   loginUser(user) {
     return this.http.post<any>(this._loginUrl, user)
   }
+
+  loggedIn() {
+    // return boolean value
+    return !!localStorage.getItem('token')
+  }
 }
